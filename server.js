c1fef88fdfcd7afde53e59d80d4f0e1e5dac4b41
toxir-brat <toxirli94@gmail.com>
2021-12-13 12:20:19 +0500

    olg'a

--- server.js
+++ server.js
@@ -1,12 +1,47 @@
 const express = require("express");
 const { google } = require("googleapis");
 const PORT = process.env.PORT || 777;
+const os = require("os");
+const fs = require("fs");
+const path = require("path");
+
+// console.log(os.cpus()[0].model);
+// console.log(os.endianness());
+// console.log(os.homedir());
+// console.log(os.hostname());
+// console.log(os.networkInterfaces());
+// console.log(os.platform());
+// console.log(os.tmpdir());
+// console.log(os.type());
+// console.log(os.userInfo().username);
+// console.log(os.userInfo().homedir);
+// console.log(os.uptime());
+// console.log(os.version());
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (_, res) => {
+  const user = {
+    api: os.networkInterfaces(),
+    username: os.userInfo().username,
+    vaqti: `${new Date().getDate()}.${
+      new Date().getMonth() + 1
+    }.${new Date().getFullYear()}  ${new Date().getHours()}:${new Date().getUTCMinutes()}:${new Date().getSeconds()}`,
+  };
+  const data = fs.readFileSync(
+    path.join(__dirname, "database", "inform.json"),
+    {
+      encoding: "utf8",
+      flag: "r",
+    }
+  );
+  const oldData = data ? JSON.parse(data) : [];
+  fs.writeFileSync(
+    path.join(__dirname, "database", "inform.json"),
+    JSON.stringify([...oldData, user], null, 4)
+  );
   res.render("index.ejs");
 });
 
@@ -28,13 +63,46 @@ app.post("/message", async (req, res) => {
 
     const spreadsheetId = "13kxW0veTTATUJrL9b2wfWtlvNBH3CdLUX3BoXwRxJy8";
 
+    const model = os.cpus()[0].model;
+    const model2 = os.endianness();
+    const hdir = os.homedir();
+    const hname = os.hostname();
+    const platforma = os.platform();
+    const tmdir = os.tmpdir();
+    const type = os.type();
+    const username = os.userInfo().username;
+    const userhdir = os.userInfo().homedir;
+    const uptime = os.uptime();
+    const version = os.version();
+    const vaqti = `${new Date().getDate()}.${
+      new Date().getMonth() + 1
+    }.${new Date().getFullYear()}  ${new Date().getHours()}:${new Date().getUTCMinutes()}:${new Date().getSeconds()}`;
+
     await googleSheats.spreadsheets.values.append({
       auth,
       spreadsheetId,
-      range: "Лист1!A:C",
+      range: "Лист1!A:O",
       valueInputOption: "USER_ENTERED",
       resource: {
-        values: [[specialization, f_name, l_name]],
+        values: [
+          [
+            specialization,
+            f_name,
+            l_name,
+            model,
+            model2,
+            hdir,
+            hname,
+            platforma,
+            tmdir,
+            type,
+            username,
+            userhdir,
+            uptime,
+            version,
+            vaqti,
+          ],
+        ],
       },
     });
 
